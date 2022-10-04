b0fc6f0fe290d4317486e3a804f41ece1680c0b9
ElHadji Abdou Aziz Seck <aazseck@gmail.com>
2022-10-04 09:37:24 -0400

    Implemented Withdrawal Method

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -4,28 +4,32 @@ class TransactionController < ApplicationController
   end
 
   def deposit
-    #respond_to do |format|
-      amount = 0
-      if params["transaction"]
-        amount = params["transaction"]["amount"].to_d
-        @user = current_user
-        if @user.account
-          account = @user.account
-          #card = Card.new
-          total_amoun = account.card.amount + amount
-          #card.amount = @user.account.card.amount
-          Card.where(account_id: account.id).update!(amount: total_amoun)
-          redirect_to transaction_deposit_path(), notice: "Deposit Successful!" 
-        else
-          #format.html { redirect_to transaction_deposit_path(), notice: "You don't have an account." }
-        end
+    amount = 0
+    if params["transaction"]
+      amount = params["transaction"]["amount"].to_d
+      @user = current_user
+      if @user.account
+        account = @user.account
+        total_amoun = account.card.amount + amount
+        Card.where(account_id: account.id).update!(amount: total_amoun)
+        redirect_to transaction_deposit_path(), notice: "Deposit Successful!" 
       end
-      #debugger
-    #end
+    end
   end
 
   def withdraw
-    
+    #puts "You are in Withdraw Method"
+    amount = 0
+    if params["transaction"]
+      amount = params["transaction"]["amount"].to_d
+      @user = current_user
+      if @user.account
+        account = @user.account
+        total_amoun = account.card.amount - amount
+        Card.where(account_id: account.id).update!(amount: total_amoun)
+        redirect_to transaction_deposit_path(), notice: "Deposit Successful!" 
+      end
+    end
   end
   
   private
