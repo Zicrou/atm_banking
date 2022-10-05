b80e23bf93b89f7b50cfd93091b8a6989778460b
ElHadji Abdou Aziz Seck <aazseck@gmail.com>
2022-10-05 16:30:41 -0400

    Set condition: User can only deposit, withdraw if he/she has an account

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -4,30 +4,38 @@ class TransactionController < ApplicationController
   end
 
   def deposit
-    amount = 0
-    if params["transaction"]
-      amount = params["transaction"]["amount"].to_d
-      @user = current_user
-      if @user.account
-        account = @user.account
-        total_amoun = account.card.amount + amount
-        Card.where(account_id: account.id).update!(amount: total_amoun)
-        redirect_to transaction_deposit_path(), notice: "Deposit Successful!" 
+    if current_user.account.nil?
+      redirect_to accounts_url, notice: "No Account Or Card"
+    else
+      amount = 0
+      if params["transaction"]
+        amount = params["transaction"]["amount"].to_d
+        @user = current_user
+        if @user.account
+          account = @user.account
+          total_amoun = account.card.amount + amount
+          Card.where(account_id: account.id).update!(amount: total_amoun)
+          redirect_to transaction_deposit_path(), notice: "Deposit Successful!" 
+        end
       end
     end
+    
   end
 
   def withdraw
-    #puts "You are in Withdraw Method"
-    amount = 0
-    if params["transaction"]
-      amount = params["transaction"]["amount"].to_d
-      @user = current_user
-      if @user.account
-        account = @user.account
-        total_amoun = account.card.amount - amount
-        Card.where(account_id: account.id).update!(amount: total_amoun)
-        redirect_to transaction_deposit_path(), notice: "Withdraw Successful!" 
+    if current_user.account.nil?
+      redirect_to accounts_url, notice: "No Account Or Card"
+    else
+      amount = 0
+      if params["transaction"]
+        amount = params["transaction"]["amount"].to_d
+        @user = current_user
+        if @user.account
+          account = @user.account
+          total_amoun = account.card.amount - amount
+          Card.where(account_id: account.id).update!(amount: total_amoun)
+          redirect_to transaction_deposit_path(), notice: "Withdraw Successful!" 
+        end
       end
     end
   end
